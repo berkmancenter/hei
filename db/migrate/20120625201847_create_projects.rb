9d464072c7b9797eb1e14c1a37a8aefc0993beca
djcp <dan@collispuro.com>
2012-06-26 11:38:12 -0400

    Project update.

--- db/migrate/20120625201847_create_projects.rb
+++ db/migrate/20120625201847_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :title, :null => false, :limit => 255
-      t.text :description, :limit => 5.kilobytes
+      t.string :description, :limit => 5.kilobytes
       t.timestamps
     end
   end
