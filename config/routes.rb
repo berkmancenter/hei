6ef9446555e5684215d31c96570db5368e55e283
djcp <dan@collispuro.com>
2012-06-27 08:42:33 -0400

    Simple UI tweaks to projects. Fake data.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Hei::Application.routes.draw do
+  resources :facets
+
   resources :projects
 
   # The priority is based upon order of creation:
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120625220946) do
+ActiveRecord::Schema.define(:version => 20120626224007) do
+
+  create_table "facets", :force => true do |t|
+    t.string   "name"
+    t.string   "context"
+    t.integer  "position"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "title",                             :null => false
