ca89c938fbf634ae533cd050d7f07cecb80acd6e
Ryan Westphal <ryan@trippingthebits.com>
2013-06-25 17:52:43 -0400

    seed test data on prepare

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :project do
     factory :project_valid do
       title 'Hei'
-      description 'Hey, Everyone: Integrate'
+      description 'Hei, Everyone: Integrate!'
       repository_url 'https://github.com/berkmancenter/devise_harvard_auth_proxy'
       news_url 'https://cyber.law.harvard.edu/projectmanagement/projects/hei/activity'
       documentation_url 'https://cyber.law.harvard.edu/projectmanagement/projects/hei/wiki'
