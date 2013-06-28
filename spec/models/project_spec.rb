8c182fcc3df5acd5ada32eaecac08f32ef17c073
Ryan Westphal <ryan@trippingthebits.com>
2013-06-28 17:04:11 -0400

    split test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe 'Project model' do
   describe 'with valid data' do
-    it {
+    it ( 'should be valid' ) {
+      p = Project.new( FactoryGirl.attributes_for( :project_valid ) );
+      p.should be_valid;
+    }
+
+    it ( 'should respond to correct attributes' ) {
       p = Project.new( FactoryGirl.attributes_for( :project_valid ) );
       p.should be_valid;
       p.should respond_to( :title );
