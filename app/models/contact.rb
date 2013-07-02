d0ddf628844f911ca7a187b69f306dc77c86748c
Anita <anita@anitapatel.com>
2013-07-02 09:31:47 -0400

    fleshed out contact and organization views, added validations and formtastic

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,11 @@ class Contact < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email, :phone, :organization_ids
   
   has_and_belongs_to_many :organizations
+  
+  validates_presence_of :first_name, :last_name
+  validates_presence_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"
+  
+  def to_s
+    "#{self.first_name} #{self.last_name}"
+  end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,10 @@ class Organization < ActiveRecord::Base
   attr_accessible :location, :name, :phone, :contact_ids
   
   has_and_belongs_to_many :contacts
+  
+  validates_presence_of :name, :phone
+  
+  def to_s
+    self.name
+  end
 end
