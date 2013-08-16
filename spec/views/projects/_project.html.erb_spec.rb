a8476111a240e8ff700fce79f47697dbee4d74ea
Ryan Westphal <ryan@trippingthebits.com>
2013-08-16 15:08:02 -0400

    finished project card style & content

--- spec/views/projects/_project.html.erb_spec.rb
+++ spec/views/projects/_project.html.erb_spec.rb
@@ -17,11 +17,25 @@ describe 'project partial' do
     should have_selector "li.project-card>a[href*='#{project_path( project )}']"
   end
 
-  it 'should have project id in link' do
-    should have_selector "li.project-card>a[data-project-id='#{project.id}']"
-  end
-
   it 'should have a header' do
     should have_selector 'h3', text: project.title
+  end
+
+  it 'should show updated_at' do
+    should have_selector 'time', text: project.updated_at.to_formatted_s( :short )
+    should have_css "time[datetime='#{project.updated_at.to_s( :db ).sub( ' ', 'T' )[ 0..-4 ]}']"
+  end
+
+  it 'should show organization' do
+    should have_selector 'div', text: project.organization.name
+  end
+
+  it 'should show progress' do
+    should have_selector 'progress'
+    should have_css "progress[value='#{project.progress}']"
+  end
+
+  it 'should show tags' do
+    should have_css '.tags li', count: 3
   end
 end
