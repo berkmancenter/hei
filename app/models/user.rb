4f2d551067fc9ce2f445b589e3a3376479ad21e8
Ryan Westphal <ryan@trippingthebits.com>
2014-08-14 16:58:49 -0400

    #5471 - do we get extra attrs from PIN?

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  def cas_extra_attributes=( extra_attributes )
+    extra_attributes.each { |n, v|
+      puts "#{n}: #{v}"
+    }
+  end
 end
